35db7257f7e56ea616f98ed304c8758b7c27ac37
Hayeong Lee <hayeong.lee@kcl.ac.uk>
2025-02-01 18:07:41 +0000

    Add SOCIETY_TYPE_CHOICES

--- uni_society/clubs/choices.py
+++ uni_society/clubs/choices.py
@@ -2,3 +2,13 @@ VISIBILITY_CHOICES = (
     (0, "Private"),
     (1, "Public"),
 )
+
+SOCIETY_TYPE_CHOICES = (
+    (, "Art"),
+    (, "Sports"),
+    (, "Culture"),
+    (, "Academic"),
+    (, "IT"),
+    (, "Language"),
+    (, "Other")
+)

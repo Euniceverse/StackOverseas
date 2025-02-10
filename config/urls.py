5479dd45b594457bf52a62f34661bd956524c626
wchen-x <cw250105@gmail.com>
2025-02-10 21:25:47 +0000

    Add paths for creating news form

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from apps.users.views import home
@@ -32,3 +34,7 @@ urlpatterns = [
     path('societies/', include('apps.societies.urls')),
     path('users/', include('apps.users.urls')),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

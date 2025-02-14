d5ea74e0d30270f6be1a12aad07e21dc9f15fcfe
Hayeong Lee <hayeong.lee@kcl.ac.uk>
2025-02-14 23:08:26 +0000

    Set serializers for event

---
+++ apps/events/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from apps.events.models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    start_datetime = serializers.SerializerMethodField()
+    end_datetime = serializers.SerialzeeMethondField()
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+    def get_start_datetime(self, obj):
+        return f"{obj.date}T{obj.start_time}"
+    
+    def get_end_datetime(self, obj):
+        if obj.ent_time:
+            return f"{obj.date}T{obj.start_time}"
+        return f"{obj.date}T{obj.start_time}"
+    

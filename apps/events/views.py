dc546db02cfd19f91d9f071bc75393145c410ae5
bellascode <ilandgrebe01@gmail.com>
2025-03-26 19:15:05 +0000

    Try fix

--- apps/events/views.py
+++ apps/events/views.py
@@ -208,7 +208,7 @@ def auto_edit_news(request, event_id):
             for news_item in instances:
                 news_item.society = society
                 news_item.is_published = True
-                news_item.save()
+                news_item.save(update_fields=['society', 'is_published', 'date_posted'])
 
             messages.success(request, "News updated and published!")
             return redirect('society_page', society_id=society.id)

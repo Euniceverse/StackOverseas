d73450901e72877d7f4656e7643df51f0de518ba
sarutafeel <feelkittipat2004@gmail.com>
2025-03-27 13:24:46 +0000

    add UI for gallery

--- apps/panels/forms.py
+++ apps/panels/forms.py
@@ -20,6 +20,11 @@ class GalleryForm(forms.ModelForm):
         model = Gallery
         fields = ['title', 'description']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = Image

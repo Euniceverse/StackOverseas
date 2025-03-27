b57e23642c9f81e8d7abcef3607e4996be506cc0
wchen-x <cw250105@gmail.com>
2025-03-27 11:08:04 +0000

    Add gallery widget

--- apps/societies/views.py
+++ apps/societies/views.py
@@ -619,6 +619,7 @@ def society_page(request, society_id):
     
     recent_polls = Poll.objects.filter(society=society).order_by("-id")[:3]
     recent_comments = society.comments.all().order_by('-created_at')[:3]
+    gallery = society.gallery_society.first()
     
     context = {
         "society": society,
@@ -631,6 +632,7 @@ def society_page(request, society_id):
         "can_manage": can_manage,
         "recent_polls": recent_polls,
         "recent_comments": recent_comments,
+        "gallery": gallery,
     }
     return render(request, "society_page.html", context)
 

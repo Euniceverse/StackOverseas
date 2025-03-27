0f4d886b11d2452864f8db99eb8508eadfccde5b
bellascode <ilandgrebe01@gmail.com>
2025-03-27 00:36:36 +0000

    Remove duplicate debug

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@h)j0-+ym+l*&l)r_qyca^#z3vr-@jawo!sna2^+(u9uy!jfwy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
